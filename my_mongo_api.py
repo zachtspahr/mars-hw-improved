d56cbf4132c9ad931e7d86d28e7d7915880ed710
Zachary Thomas Spahr <zachtspahr@gmail.com>
2022-04-13 15:41:57 -0400

    add_regression
    initial functions added to app with htmls sent over

--- my_mongo_api.py
+++ my_mongo_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, url_for, request, redirect
 from flask_pymongo import PyMongo
 import re
 import json
@@ -12,6 +12,17 @@ from os import environ
 import pandas as pd
 from bs4 import BeautifulSoup
 import urllib.request
+import urllib.request
+import math
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+import numpy as np
+from patsy import dmatrices
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+import random
+from random import randrange
 
 #from "/Users/zspahr/Desktop/Bootcamp/config.py" import mongo
 
@@ -46,6 +57,9 @@ class JSONEncoder(json.JSONEncoder):
 
 
 
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
 
 @zek_app.route("/")
 def index():
@@ -174,6 +188,349 @@ def api_house_pop_vote_map():
 def insert():
     #db.db.collection.insert_one({"name": "John"})
     return "Connected to the data base!"
+
+@zek_app.route("/cds_model_president")
+def good_ole_data():
+    some_data = pd.read_csv("https://raw.githubusercontent.com/zachtspahr/zachtspahr.github.io/master/models_edited_president.csv")
+    return some_data.to_html()
+
+@zek_app.route('/choose_regression', methods=['GET', 'POST'])
+def regression_output():
+  selected_variables = "zek"
+  if request.method == 'POST':
+    form = request.form
+    dadumle = pd.DataFrame()
+    selected_variables = var_list(form)
+    #table = vif_tables(dadumle)
+    #print(variance_factor)
+    return render_template('index.html',selected_variables = selected_variables)
+    #return redirect(url_for('success',name = user))
+  else:
+    selected_variables=("","","","","","","","","","","","","","","","","","","","","","","","")
+    return render_template('index.html',selected_variables = selected_variables)
+def var_list(form):
+  #fix exceptions here
+  reg_vars = []  
+  cook_pvi = request.form['2018_Cook_PVI_Score']
+  white_ba = request.form["white_ba_pct_pop"]
+  white_no_ba = request.form['white_non_college_pct_pop']
+  black_pct = request.form['black_pct_pop']
+  asian_pct = request.form['asian_pct_pop']
+  white_pct = request.form['white_pct_pop']
+  hispanic_pct = request.form['hispanic_pct_pop']
+  other_race_pct = request.form['other_race_pct_pop']
+  black_ba = request.form['black_ba_pct_dis']
+  black_no_ba = request.form['black_non_college_pct_dis']
+  hispanic_ba  = request.form['hispanic_ba_pct_pop']
+  hispanic_no_ba = request.form['hispanic_non_college_pct_pop']
+  asian_ba = request.form['asian_ba_pct_pop']
+  asian_no_ba = request.form['asian_non_college_pct_pop']
+  city_lab = request.form['City_Lab_Congressional_Index_Pure rural']
+
+  if cook_pvi=="0":
+      print("cook pvi not checked")
+  else:
+        reg_vars.append(cook_pvi)
+  
+  if white_no_ba=="0":
+      print("white_no_ba not checked")
+  else:
+        reg_vars.append(white_no_ba)
+  
+  if white_ba=="0":
+      print("white_ba not checked")
+  else:
+        reg_vars.append(white_ba)
+  if black_no_ba=="0":
+      print("black_no_ba not checked")
+  else:
+        reg_vars.append(black_no_ba)
+  
+  if black_ba=="0":
+      print("black_ba not checked")
+  else:
+        reg_vars.append(black_ba)
+    
+  if hispanic_no_ba=="0":
+      print("hispanic_no_ba not checked")
+  else:
+        reg_vars.append(hispanic_no_ba)
+  
+  if hispanic_ba=="0":
+      print("hispanic_ba not checked")
+  else:
+        reg_vars.append(hispanic_ba)
+
+  if asian_no_ba=="0":
+      print("asian_no_ba not checked")
+  else:
+        reg_vars.append(asian_no_ba)
+  
+  if asian_ba=="0":
+      print("asian_ba not checked")
+  else:
+        reg_vars.append(asian_ba)
+
+  if white_pct=="0":
+      print("white_pct not checked")
+  else:
+        reg_vars.append(white_pct)
+
+  if black_pct=="0":
+      print("black_pct not checked")
+  else:
+        reg_vars.append(black_pct)
+  
+  if hispanic_pct=="0":
+      print("hispanic_pct not checked")
+  else:
+        reg_vars.append(hispanic_pct)
+    
+  if asian_pct=="0":
+      print("asian_pct not checked")
+  else:
+        reg_vars.append(asian_pct)
+        
+  if other_race_pct=="0":
+      print("other_race_pct not checked")
+  else:
+        reg_vars.append(other_race_pct)
+  
+  if city_lab=="0":
+      print("city_lab not checked")
+  else:
+        reg_vars.append("City_Lab_Congressional_Index_Dense suburban")
+        reg_vars.append("City_Lab_Congressional_Index_Pure urban")
+        reg_vars.append("City_Lab_Congressional_Index_Rural-suburban mix")
+        reg_vars.append("City_Lab_Congressional_Index_Sparse suburban")
+        reg_vars.append("City_Lab_Congressional_Index_Urban-suburban mix")
+  letters =""
+  for i in range(5):
+    randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
+    letters= letters= letters + randomUpperLetter
+  numbers = randrange(0, 10000000)
+  new_id= f'{letters}{numbers}'
+  vars_dict = {'_id':new_id,
+      "selected_variables":reg_vars}
+  print(vars_dict)
+  CONNECTION_STRING = f'mongodb+srv://{username}:{mot_de_passe}@cluster0-zgmov.mongodb.net/test?retryWrites=true&w=majority'
+  print(username)
+  client = pymongo.MongoClient(CONNECTION_STRING)
+  #print(username)
+  db = client.get_database('geojsons')
+  #db.user_variables.delete_many({})
+  user_variables = db.regression_variables.find()    
+  db.user_variables.insert_one(vars_dict)
+
+ 
+  # Note that I need to make some checks here to make sure number of variables is computed and avoid errors
+  # with exceptions   Issues are if no variables are selected for everything and 1 variable selected for vifs
+  
+  
+  recent_variables = reg_vars
+  print(len(recent_variables))
+
+  some_data = pd.read_csv("https://raw.githubusercontent.com/zachtspahr/zachtspahr.github.io/master/models_edited_president.csv")
+  if len(recent_variables)==0:
+      reg_variables=("No variables selected","No variables selected",
+      "No variables selected","","","","","","","","","","","","")
+      print("no variables selected")
+  elif len(recent_variables)==1:
+      vif_list=[]
+      vif_list.append(None) 
+      vif_list.append(None)
+      X = some_data[recent_variables]
+      y = some_data["Biden_Vote"]
+      Z= some_data["Trump_Vote_20"]
+      zek = some_data["Biden_margin"]
+      X2 = sm.add_constant(X)
+      est = sm.OLS(y, X2)
+      est2 = est.fit()
+      est2_html = est2.summary().as_html()
+      est3 = sm.OLS(Z, X2)
+      est4= est3.fit()
+      est4_html = est4.summary().as_html()
+      est5 = sm.OLS(zek, X2)
+      est6= est5.fit()
+      est6_html = est6.summary().as_html()
+      columns=['Variables','Coefficients','Standard Error',"T Stat","P-Value","Lower 95%","Upper 95%"]
+
+      bob = pd.read_html(est2.summary().tables[1].as_html())[0]
+      bob = bob.rename(columns={0: columns[0], 1: columns[1],2: columns[2], 3: columns[3],4: columns[4], 5: columns[5],
+                   6:columns[6]})
+      bob = bob.iloc[1:,:]
+      bob["vifs"]=vif_list
+      bob["P-Value"]=bob["P-Value"].astype(float)
+      bob["vifs"]=bob["vifs"].astype(float)
+      bob.loc[bob['P-Value'] <= .05, 'Significant?'] = 'Yes' 
+      bob.loc[bob['P-Value'] > .05, 'Significant?'] = 'No'
+      bob.loc[bob['vifs'] <= 5, 'Covariance Problem?'] = 'No' 
+      bob.loc[bob['vifs'] > 5, 'Covariance Problem?'] = 'Yes'
+      bob = bob.to_html()
+
+      bob2 = pd.read_html(est4.summary().tables[1].as_html())[0]
+      bob2 = bob2.rename(columns={0: columns[0], 1: columns[1],2: columns[2], 3: columns[3],4: columns[4], 5: columns[5],
+                   6:columns[6]})
+      bob2 = bob2.iloc[1:,:]
+      bob2["vifs"]=vif_list
+      bob2["P-Value"]=bob2["P-Value"].astype(float)
+      bob2["vifs"]=bob2["vifs"].astype(float)
+      bob2.loc[bob2['P-Value'] <= .05, 'Significant?'] = 'Yes' 
+      bob2.loc[bob2['P-Value'] > .05, 'Significant?'] = 'No'
+      bob2.loc[bob2['vifs'] <= 5, 'Covariance Problem?'] = 'No' 
+      bob2.loc[bob2['vifs'] > 5, 'Covariance Problem?'] = 'Yes'
+      bob2 = bob2.to_html()
+
+      bob3 = pd.read_html(est6.summary().tables[1].as_html())[0]
+      bob3 = bob3.rename(columns={0: columns[0], 1: columns[1],2: columns[2], 3: columns[3],4: columns[4], 5: columns[5],
+                   6:columns[6]})
+      bob3 = bob3.iloc[1:,:]
+      bob3["vifs"]=vif_list
+      bob3["P-Value"]=bob3["P-Value"].astype(float)
+      bob3["vifs"]=bob3["vifs"].astype(float)
+      bob3.loc[bob3['P-Value'] <= .05, 'Significant?'] = 'Yes' 
+      bob3.loc[bob3['P-Value'] > .05, 'Significant?'] = 'No'
+      bob3.loc[bob3['vifs'] <= 5, 'Covariance Problem?'] = 'No' 
+      bob3.loc[bob3['vifs'] > 5, 'Covariance Problem?'] = 'Yes'
+      bob3 = bob3.to_html()
+      params_df = pd.DataFrame(est2.params).reset_index()
+      params_df2 = pd.DataFrame(est4.params).reset_index()
+      params_df3 = pd.DataFrame(est6.params).reset_index()
+      constant =truncate(params_df.iloc[0,1],3)
+      constant2 = truncate(params_df2.iloc[0,1],3)
+      constant3 = truncate(params_df3.iloc[0,1],3)
+      rsq1= truncate(est2.rsquared,3)
+      rsq2= truncate(est4.rsquared,3)
+      rsq3= truncate(est6.rsquared,3)
+      zek=f"{constant}"
+      zek2 = f"{constant2}"
+      zek3=f"{constant3}" 
+      for i in range(len(params_df["index"].tolist())-1):
+          zek= zek + f' + {params_df.loc[i+1,0].round(2)} * {params_df.loc[i+1,"index"]}'
+          zek2= zek2 + f' + {params_df2.loc[i+1,0].round(2)} * {params_df2.loc[i+1,"index"]}'
+          zek3= zek3 + f' + {params_df3.loc[i+1,0].round(2)} * {params_df3.loc[i+1,"index"]}'
+      f_pval1 = truncate(est2.f_pvalue,3)
+      if f_pval1 <=0.05:
+          model_sig1 = "Overall Model is Signficant"
+      else:
+          model_sig1= "Overall Model is not significant"
+      f_pval2 = truncate(est4.f_pvalue,3)
+      if f_pval2 <=0.05:
+          model_sig2 = "Overall Model is Signficant"
+      else:
+          model_sig2= "Overall Model is not significant"
+      f_pval3 =truncate(est6.f_pvalue,3)
+      if f_pval3 <=0.05:
+          model_sig3 = "Overall Model is Signficant"
+      else:
+          model_sig3= "Overall Model is not significant"
+      reg_variables=(bob,bob2,bob3,zek,zek2,zek3,rsq1,rsq2,rsq3,f_pval1, f_pval2, f_pval3,model_sig1,
+      model_sig2,model_sig3)
+      print("Only One Variable Selected")
+  else:
+      print("Multiple variables selected")
+      X = some_data[recent_variables]
+      vif = pd.DataFrame()
+      vif["VIF Factor"] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
+      vif["features"] = X.columns
+      vif_dict = {"vifs":vif.to_dict(orient="records")}
+      vifs = db.vif_tables.find()    
+      db.vifs.insert_one(vif_dict)
+      vif_list=[]
+      vif_list.append(None)
+      for new_vif in vif.iloc[:,0].to_list():
+          vif_list.append(new_vif)
+      X = some_data[recent_variables]
+      y = some_data["Biden_Vote"]
+      Z= some_data["Trump_Vote_20"]
+      zek = some_data["Biden_margin"]
+      X2 = sm.add_constant(X)
+      est = sm.OLS(y, X2)
+      est2 = est.fit()
+      est2_html = est2.summary().as_html()
+      est3 = sm.OLS(Z, X2)
+      est4= est3.fit()
+      est4_html = est4.summary().as_html()
+      est5 = sm.OLS(zek, X2)
+      est6= est5.fit()
+      est6_html = est6.summary().as_html()
+      columns=['Variables','Coefficients','Standard Error',"T Stat","P-Value","Lower 95%","Upper 95%"]
+
+      bob = pd.read_html(est2.summary().tables[1].as_html())[0]
+      bob = bob.rename(columns={0: columns[0], 1: columns[1],2: columns[2], 3: columns[3],4: columns[4], 5: columns[5],
+                   6:columns[6]})
+      bob = bob.iloc[1:,:]
+      bob["vifs"]=vif_list
+      bob["P-Value"]=bob["P-Value"].astype(float)
+      bob["vifs"]=bob["vifs"].astype(float)
+      bob.loc[bob['P-Value'] <= .05, 'Significant?'] = 'Yes' 
+      bob.loc[bob['P-Value'] > .05, 'Significant?'] = 'No'
+      bob.loc[bob['vifs'] <= 5, 'Covariance Problem?'] = 'No' 
+      bob.loc[bob['vifs'] > 5, 'Covariance Problem?'] = 'Yes'
+      bob = bob.to_html()
+
+      bob2 = pd.read_html(est4.summary().tables[1].as_html())[0]
+      bob2 = bob2.rename(columns={0: columns[0], 1: columns[1],2: columns[2], 3: columns[3],4: columns[4], 5: columns[5],
+                   6:columns[6]})
+      bob2 = bob2.iloc[1:,:]
+      bob2["vifs"]=vif_list
+      bob2["P-Value"]=bob2["P-Value"].astype(float)
+      bob2["vifs"]=bob2["vifs"].astype(float)
+      bob2.loc[bob2['P-Value'] <= .05, 'Significant?'] = 'Yes' 
+      bob2.loc[bob2['P-Value'] > .05, 'Significant?'] = 'No'
+      bob2.loc[bob2['vifs'] <= 5, 'Covariance Problem?'] = 'No' 
+      bob2.loc[bob2['vifs'] > 5, 'Covariance Problem?'] = 'Yes'
+      bob2 = bob2.to_html()
+
+      bob3 = pd.read_html(est6.summary().tables[1].as_html())[0]
+      bob3 = bob3.rename(columns={0: columns[0], 1: columns[1],2: columns[2], 3: columns[3],4: columns[4], 5: columns[5],
+                   6:columns[6]})
+      bob3 = bob3.iloc[1:,:]
+      bob3["vifs"]=vif_list
+      bob3["P-Value"]=bob3["P-Value"].astype(float)
+      bob3["vifs"]=bob3["vifs"].astype(float)
+      bob3.loc[bob3['P-Value'] <= .05, 'Significant?'] = 'Yes' 
+      bob3.loc[bob3['P-Value'] > .05, 'Significant?'] = 'No'
+      bob3.loc[bob3['vifs'] <= 5, 'Covariance Problem?'] = 'No' 
+      bob3.loc[bob3['vifs'] > 5, 'Covariance Problem?'] = 'Yes'
+      bob3 = bob3.to_html()
+
+      params_df = pd.DataFrame(est2.params).reset_index()
+      params_df2 = pd.DataFrame(est4.params).reset_index()
+      params_df3 = pd.DataFrame(est6.params).reset_index()
+      constant = truncate(params_df.iloc[0,1],3)
+      constant2 = truncate(params_df2.iloc[0,1],3)
+      constant3 = truncate(params_df3.iloc[0,1],3)
+      rsq1= truncate(est2.rsquared,3)
+      rsq2= truncate(est4.rsquared,3)
+      rsq3= truncate(est6.rsquared,3)
+      zek=f"{constant}"
+      zek2 = f"{constant2}"
+      zek3=f"{constant3}" 
+      for i in range(len(params_df["index"].tolist())-1):
+          zek= zek + f' + {params_df.loc[i+1,0].round(2)} * {params_df.loc[i+1,"index"]}'
+          zek2= zek2 + f' + {params_df2.loc[i+1,0].round(2)} * {params_df2.loc[i+1,"index"]}'
+          zek3= zek3 + f' + {params_df3.loc[i+1,0].round(2)} * {params_df3.loc[i+1,"index"]}'
+      f_pval1 = truncate(est2.f_pvalue,3)
+      if f_pval1 <=0.05:
+          model_sig1 = "Overall Model is Signficant"
+      else:
+          model_sig1= "Overall Model is not significant"
+      f_pval2 = truncate(est4.f_pvalue,3)
+      if f_pval2 <=0.05:
+          model_sig2 = "Overall Model is Signficant"
+      else:
+          model_sig2= "Overall Model is not significant"
+      f_pval3 =truncate(est6.f_pvalue,3)
+      if f_pval3 <=0.05:
+          model_sig3 = "Overall Model is Signficant"
+      else:
+          model_sig3= "Overall Model is not significant"
+      reg_variables=(bob,bob2,bob3,zek,zek2,zek3,rsq1,rsq2,rsq3,f_pval1, f_pval2, f_pval3,model_sig1,
+      model_sig2,model_sig3)
+  # this is tuple with 3 data frames and 3 equations, 3 r squared value
+
+  return (reg_variables)
 
 if __name__ == "__main__":
     zek_app.run(debug=True)
